81adfd5929859ac88c5678079d6c767fa4d10e61
Alejandro Santiago <44524995+alestiago@users.noreply.github.com>
2020-02-24 16:18:29 +0000

    Added Adafruit LCD RGB Shield custom char code example

---
+++ examples/Adafruit_RGBLCDShield.ino
@@ -0,0 +1,48 @@
+/********************* 
+
+Example code for defining a custom character for the 
+Adafruit RGB Character LCD Shield.
+
+This code displays the custom character on the shield.
+
+**********************/
+
+// Includes the Adafruit RGB library code:
+#include <Wire.h>
+#include <Adafruit_RGBLCDShield.h>
+#include <utility/Adafruit_MCP23017.h>
+
+// Define the global variable Adafruit LCD object:
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
+
+// Define your custom character:
+byte smilyFace[] = {
+    B00000,
+    B01010,
+    B01010,
+    B00000,
+    B10001,
+    B01110,
+    B00000,
+    B00000
+};
+
+void setup(){
+  // Set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+   // Creates new character smilyFace as 0
+   // (Note: alternative you can use a macro [eg #define SMILYCHAR 0] for legibility)
+  lcd.createChar(0, smilyFace);
+  
+  // Sets the cursor to column 2, row 0 (zero-based)
+  lcd.setCursor(2, 0);
+
+  // Prints the character, refering to it as 0, because it was declared
+  // as 0 before.
+  // (Note: we use .write to print characters and .print to print strings)
+  lcd.write(0);
+}
+
+void loop(){
+}
