a6c10397bd22e0ec21b358e8b01b6b4ffa1692c3
Alejandro Santiago <44524995+alestiago@users.noreply.github.com>
2020-02-28 15:38:29 +0000

    Added lincese to header of code

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,11 +1,30 @@
+/*
+{{ Chareditor.com }}
+Copyright (C) {{ 2020 }}  {{ alestiago }}
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+
 /**
  * Contains all JavaScript functions.
  *
  * @file   This files defines all functions.
  * @author Alejandro Santiago (@alestiago)
- * @since  27.02.2020
+ * @license GPL-3.0
  */
-
+ 
 /* global selectedLCD */
 
 // Load content from memory
