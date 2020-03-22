a91e3d8e00a4f1e81f6493a7737ef769ab6bba2b
Alejandro Santiago <44524995+alestiago@users.noreply.github.com>
2020-03-22 19:48:11 +0000

    Modified function docstrings

--- scripts/editor/options-navigation.js
+++ scripts/editor/options-navigation.js
@@ -6,7 +6,10 @@
  Toggling Navigation Bar Menus
 *******************************/
 function showThisOption (optionIcon) {
-  /* Sets the icon to active and shows the corresponding option menu */
+  /**
+   * Sets the icon to active and shows the corresponding option menu
+   * @param {Object} optionIcon
+   */
   deactivatePreviousOptionIcon();
   activateOptionIcon(optionIcon);
   
@@ -15,27 +18,33 @@ function showThisOption (optionIcon) {
 }
 
 function deactivatePreviousOptionIcon () {
-  /* Removes the active class name from the previous icon */
+  /** Removes the active class name from the previous icon */
   var previousIcon = document.getElementsByClassName("active")[0];
   previousIcon.className = null;
   optionIconUnHovered(previousIcon);
 }
 
 function activateOptionIcon (optionIcon) {
-  /* Sets the active class name for this icon */
+  /**
+   * Sets the active class name for this icon
+   * @param {Object} optionIcon
+   */
   optionIconHovered(optionIcon);
   optionIcon.className = "active";
 }
 
 function hidePreviousOptionMenu () {
-  /* Hides and removes the shown class name of the previous option menu */
+  /** Hides and removes the shown class name of the previous option menu */
   var previousMenu = document.getElementsByClassName("shown")[0];
   previousMenu.hidden = true;
   previousMenu.className = null;
 }
 
 function showOptionMenu (optionIcon) {
-  /* Shows and sets the shown class name to option menu of the icon */
+  /**
+   * Shows and sets the shown class name to option menu of the icon
+   * @param {Object} optionIcon
+   */
   var thisOptionName = (optionIcon.id).split("-")[0];
   var showOption = "options-bar__options__" + thisOptionName;
   showOption = document.getElementById(showOption);
@@ -48,7 +57,10 @@ function showOptionMenu (optionIcon) {
  Image Handling of Option Icons
 *******************************/
 function optionIconHovered(optionIcon){
-  /* Changes image when hovered */
+  /**
+   * Changes image when hovered
+   * @param {Object} optionIcon
+   */
   if (optionIcon.className != "active") {
     var hoveredImage = "images/editor-icons/" + optionIcon.id + "__hovered.png";
     optionIcon.src = hoveredImage
@@ -56,7 +68,10 @@ function optionIconHovered(optionIcon){
 }
 
 function optionIconUnHovered(optionIcon){
-  /* Changes image when stoped hovered */
+  /**
+   * Changes image when stopped hovered
+   * @param {Object} optionIcon
+   */
   if (optionIcon.className != "active") {
     var unHoveredImage = "images/editor-icons/" + optionIcon.id + ".png";
     optionIcon.src = unHoveredImage;
